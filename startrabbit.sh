a73fee12b41d67888f1d4de622e6d73f73af1495
Shailesh Patel <shailesh@aboutobjects.com>
2020-05-12 17:56:29 -0400

    add more info

--- startrabbit.sh
+++ startrabbit.sh
@@ -20,21 +20,23 @@ echo "Starting rabbitMQ server..."
 if [ -z "$CLUSTERED" ]; then
 	# if not clustered then start it normally as if it is a single server
 	/usr/sbin/rabbitmq-server &
+    echo "Started rabbitMQ pid file is /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid"
 	rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 	change_default_user	
+    echo "tailing tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log"
 	tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 else
 	if [ -z "$CLUSTER_WITH" ]; then
 		# If clustered, but cluster with is not specified then again start normally, could be the first server in the
 		# cluster
 		/usr/sbin/rabbitmq-server&
-        echo "Started rabbitMQ pid file is /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid"
+        echo "Started rabbitMQ CLUSTERED pid file is /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid"
 		rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
         echo "tailing tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log"
 		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	else
 		/usr/sbin/rabbitmq-server &
-        echo "Started rabbitMQ cluster with mode... pid file is /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid"
+        echo "Started rabbitMQ CLUSTER_WITH mode... pid file is /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid"
 		rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 		rabbitmqctl stop_app
 		if [ -z "$RAM_NODE" ]; then
